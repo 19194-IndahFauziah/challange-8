65a80580f2ab818e5b6d18c125ab3805ef6e5dec
Indah Fauziah <indahfauziah143@gmail.com>
2022-06-10 13:48:17 +0700

    new initial

--- config/database.js
+++ config/database.js
@@ -1,24 +1,14 @@
 const {
-<<<<<<< HEAD
   DB_USER = '',
   DB_PASSWORD = '',
   DB_NAME = 'bcr',
   DB_HOST = '127.0.0.1',
-  DB_PORT = '5432',
-=======
-  DB_USER = "",
-  DB_PASSWORD = "",
-  DB_NAME = "bcr",
-  DB_HOST = "127.0.0.1",
-  DB_PORT = "5432",
->>>>>>> 5aba1e2839d996bfeb98e339bc405bc64b38d4a7
-} = process.env;
-
+  DB_PORT = '5432'
+} = process.env
 module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-<<<<<<< HEAD
     database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
@@ -26,20 +16,13 @@ module.exports = {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
-      },
-    },
-=======
-    database: `${DB_NAME}_development`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
->>>>>>> 5aba1e2839d996bfeb98e339bc405bc64b38d4a7
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-<<<<<<< HEAD
     database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
@@ -47,20 +30,13 @@ module.exports = {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
-      },
-    },
-=======
-    database: `${DB_NAME}_test`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
->>>>>>> 5aba1e2839d996bfeb98e339bc405bc64b38d4a7
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-<<<<<<< HEAD
     database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
@@ -68,17 +44,8 @@ module.exports = {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
-
-};
-=======
-    database: `${DB_NAME}_production`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
+        rejectUnauthorized: false
+      }
+    }
   }
 }
->>>>>>> 5aba1e2839d996bfeb98e339bc405bc64b38d4a7
